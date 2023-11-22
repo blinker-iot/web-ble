dbfb91574f83085e47ebd2aaebb205e0aa645d81
coloz <coloz999@gmail.com>
2023-11-22 21:56:13 +0800

    更新

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: '', redirectTo: 'connect', pathMatch: 'full' },
     { path: 'connect', loadComponent: () => import('./pages/connect/connect.component').then(m => m.ConnectComponent) },
+    { path: 'light', loadComponent: () => import('./pages/light-page/light-page.component').then(m => m.LightPageComponent) },
     { path: 'joystick', loadComponent: () => import('./pages/joystick-page/joystick-page.component').then(m => m.JoystickPageComponent) },
     { path: 'smarthome', loadComponent: () => import('./pages/smarthome-page/smarthome-page.component').then(m => m.SmarthomePageComponent) },
     { path: 'farming', loadComponent: () => import('./pages/farming-page/farming-page.component').then(m => m.FarmingPageComponent) },
---
+++ src/app/pages/light-page/light-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-light-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './light-page.component.html',
+  styleUrl: './light-page.component.scss'
+})
+export class LightPageComponent {
+
+}
