06d883a9c39bc08f130d0a6cd5557d5265d7020f
coloz <coloz999@gmail.com>
2023-12-02 21:01:45 +0800

    更新

--- src/app/components/data-box/data-box.component.ts
+++ src/app/components/data-box/data-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,6 +9,8 @@ import { CommonModule } from '@angular/common';
   styleUrl: './data-box.component.scss'
 })
 export class DataBoxComponent {
+  @Input() config;
+
   constructor() {
 
   }
--- src/app/pages/farming-page/farming-page.component.ts
+++ src/app/pages/farming-page/farming-page.component.ts
@@ -12,5 +12,75 @@ import { DataBoxComponent } from '../../components/data-box/data-box.component';
   styleUrl: './farming-page.component.scss'
 })
 export class FarmingPageComponent {
+  items = [
+    {
+      name: '环境温度',
+      icon: 'iconfont icon-temperature',
+      unit: '℃',
+      key: 'temperature'
+    }, {
+      name: '空气湿度',
+      icon: 'iconfont icon-humidity',
+      unit: '%',
+      key: 'humidity'
+    }, {
+      name: '光照',
+      icon: 'iconfont icon-sun',
+      unit: 'LX',
+      key: 'light'
+    }, {
+      name: '气压',
+      icon: 'iconfont icon-pressure',
+      unit: 'pa',
+      key: 'pressure'
+    }, {
+      name: '风向',
+      icon: 'iconfont icon-wind-direction',
+      unit: '°',
+      key: 'windDirection'
+    }, {
+      name: '风速',
+      icon: 'iconfont icon-wind-speed',
+      unit: 'm/s',
+      key: 'windSpeed'
+    }, {
+      name: '水质',
+      icon: 'iconfont icon-water',
+      unit: 'mg/L',
+      key: 'waterQuality'
+    }, {
+      name: '降雨量',
+      icon: 'iconfont icon-rainfall',
+      unit: 'mm/h',
+      key: 'rainfall'
+    }
+  ]
 
+  items2 = [
+    {
+      name: '灌溉',
+      icon: 'iconfont icon-pipe',
+      key: 'irrigation'
+    }, {
+      name: '通风',
+      icon: 'iconfont icon-fan',
+      key: 'fan'
+    }, {
+      name: '补光灯',
+      icon: 'iconfont icon-light2',
+      key: 'light'
+    }, {
+      name: '风车',
+      icon: 'iconfont icon-windmill',
+      key: 'windmill'
+    }, {
+      name: '功能1',
+      icon: 'iconfont icon-setting',
+      key: 'c1'
+    }, {
+      name: '功能2',
+      icon: 'iconfont icon-setting',
+      key: 'c2'
+    }
+  ]
 }
